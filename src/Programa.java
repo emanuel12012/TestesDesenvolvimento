e7d8fc9ee4cf9a3810b568cae3db5e1197f03363
emanuel12012 <68973382+emanuel12012@users.noreply.github.com>
2022-03-24 17:54:29 -0300

    Update Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -3,14 +3,6 @@ public class Programa {
 	public static void main(String[] args) {
 
 		System.out.println("Olá novo teste");
-
-		int idade;
-		idade = 37;
-
-		System.out.print("Sua idade é: " + idade);
-
-		idade += 23;
-		System.out.print("\n\nAgora sua idade é: " + idade);
 	}
 
 }
